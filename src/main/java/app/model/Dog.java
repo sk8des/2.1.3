8a3d9b9dbcf208f7e021f7b542e90a76939ac3b0
sk8des <102260911+sk8des@users.noreply.github.com>
2022-04-08 16:34:49 +0300

    First Commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,14 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal{
+
+    @Override
+    public String toString() {
+        return "I'm a Dog";
+    }
+}
